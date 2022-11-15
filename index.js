72f9f0f20b14b6bf6f7d0dca12f4b8a40f74f0cb
eugene <eugeneholenstein@gmail.com>
2022-11-15 22:03:41 +1100

    completed Strings Lab

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code in this file!
+const currentUser = "Chewy";
+// const welcomeMessage = 'Welcome to Flatbook, '+currentUser+'!';
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = `${welcomeMessage.toUpperCase()}, ${currentUser}!`;
+const shortGreeting = `Welcome, ${currentUser[0]}!`;
